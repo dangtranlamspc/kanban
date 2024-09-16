56e416324a2648bcb78cf143260e014a88909d83
dangtranlamspc <tranlam@spchcmc.vn>
2024-09-16 09:11:26 +0700

    ngay 3

--- src/apis/axiosClient.ts
+++ src/apis/axiosClient.ts
@@ -3,7 +3,7 @@ import { error } from "console";
 import { config } from "process";
 import queryString from "query-string";
 
-const baseURL = `http://192.168.102.252:3001`;
+const baseURL = `http://192.168.100.17:3001`;
 
 const axiosClient = axios.create({
     baseURL : baseURL,
@@ -20,15 +20,18 @@ axiosClient.interceptors.request.use(async (config : any) => {
     return config;
 })
 
-axios.interceptors.response.use(res => {
-    if (res.data && res.status >= 200 && res.status < 300) {
-        return res.data;
-    }else{
-        return Promise.reject(res.data)
+axiosClient.interceptors.response.use(
+    (res) => {
+        if (res.data && res.status >= 200 && res.status < 300) {
+            return res.data;
+        }else{
+            return Promise.reject(res.data)
+        }
+    }, 
+    (error) => {
+        const {response} = error;
+        return Promise.reject(response.data);
     }
-}, (error) => {
-    const {response} = error;
-    return Promise.reject(response.data)
-})
+);
 
 export default axiosClient
